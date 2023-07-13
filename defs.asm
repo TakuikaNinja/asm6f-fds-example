d077a69e2ee2caf36639b0ea6c0ee32a52c1b058
TakuikaNinja <takuikaninja@gmail.com>
2023-07-13 14:59:53 +1200

    Add defines

---
+++ defs.asm
@@ -0,0 +1,91 @@
+; defines
+; NES hardware defines
+
+PPU_CTRL = $2000
+PPU_MASK = $2001
+PPU_STATUS = $2002
+PPU_OAM_ADDR = $2003
+PPU_OAM_DATA = $2004
+PPU_SCROLL = $2005
+PPU_ADDR = $2006
+PPU_DATA = $2007
+
+DMC_FREQ = $4010
+DMC_RAW = $4011
+DMC_START = $4012
+DMC_LEN = $4013
+OAM_DMA = $4014
+SND_CHN = $4015
+
+JOY1 = $4016
+JOY2 = $4017
+
+; FDS hardware defines
+
+FDS_IRQ_TIMER_LOW = $4020
+FDS_IRQ_TIMER_HI  = $4021
+FDS_IRQ_TIMER_CTRL = $4022
+FDS_IO_ENABLE = $4023
+FDS_WRITE_DATA = $4024
+FDS_CTRL = $4025
+FDS_EXT = $4026 ; connector on back of RAM adaptor
+FDS_STATUS = $4030
+FDS_READ_DATA = $4031
+FDS_DRIVE_STATUS = $4032
+FDS_BATTERY_EXT = $4033 ; bit 7 = battery, rest are shared with FDS_EXT
+
+; FDS BIOS defines
+
+LoadFiles = $e1f8
+AppendFile = $e237
+WriteFile = $e239
+CheckFileCount = $e2b7
+AdjustFileCount = $e2bb
+SetFileCount1 = $e301
+SetFileCount = $e305
+GetDiskInfo = $e32a
+
+CheckDiskHeader = $e445
+GetNumFiles = $e484
+SetNumFiles = $e492
+FileMatchTest = $e4a0
+SkipFiles = $e4da
+
+Delay132 = $e149
+Delayms = $e153
+DisPFObj = $e161
+EnPFObj = $e16b
+DisObj = $e171
+EnObj = $e178
+DisPF = $e17e
+EnPF = $e185
+VINTWait = $e1b2
+VRAMStructWrite = $e7bb
+FetchDirectPointer = $e844
+WriteVRAMBuffer = $e86a
+ReadVRAMBuffer = $e8b3
+PrepareVRAMString = $e8d2
+PrepareVRAMStrings = $e8e1
+GetVRAMBufferByte = $e94f
+Pixel2NamConv = $e97d
+Nam2PixelConv = $e997
+Random = $e9b1
+SpriteDMA = $e9c8
+CounterLogic = $e9d3
+
+ReadPads = $e9eb
+ReadDownPads = $ea1a
+ReadOrDownPads = $ea1f
+ReadDownVerifyPads = $ea36
+ReadOrDownVerifyPads = $ea4c
+ReadDownExpPads = $ea68
+
+VRAMFill = $ea84
+MemFill = $ead2
+SetScroll = $eaea
+JumpEngine = $eafd
+ReadKeyboard = $eb13
+LoadTileset = $ebaf
+
+unk_EC22 = $ec22 ; apparently adds an object to OAM
+
